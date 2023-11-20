efc9a5e71a3df12ee2a70d82879e1e8fe2ccadae
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2023-11-21 03:38:23 +0500

    Updated the UI

--- script.js
+++ script.js
@@ -4,13 +4,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     rollNumberInput.addEventListener('keydown', function (event) {
         if (event.key === 'Enter') {
-            // Prevent the default form submission behavior
             event.preventDefault();
             downloadSlips();
         }
     });
 });
-
 
 function validateRollNumber(rollNumber) {
     const rollNumberPattern = /^[0-9]{5,6}$/;
@@ -19,27 +17,25 @@ function validateRollNumber(rollNumber) {
 
 function downloadSlips() {
     const rollNumber = document.getElementById('rollNumber').value;
+    const resultMessage = document.getElementById('resultMessage');
 
     if (!rollNumber) {
-        alert('Please enter a roll number.');
+        resultMessage.innerHTML = 'Please enter a roll number.';
         return;
     }
 
     if (!validateRollNumber(rollNumber)) {
-        alert('Please enter a valid 5 to 6 digit roll number.');
+        resultMessage.innerHTML = 'Please enter a valid 5 to 6 digit roll number.';
         return;
     }
 
     const apiUrl = `https://api_last-1-j0851899.deta.app/download_slip?roll_no=${rollNumber}`;
 
-    // Display loading message
-    document.getElementById('resultMessage').innerHTML = 'Downloading...';
+    resultMessage.innerHTML = 'Downloading...';
 
-    // Create form data
     const formData = new FormData();
     formData.append('roll_no', rollNumber);
 
-    // Make the POST request
     fetch(apiUrl, {
         method: 'POST',
         body: formData,
@@ -47,41 +43,32 @@ function downloadSlips() {
         .then((response) => {
             console.log('Response status:', response.status);
             if (!response.ok) {
-                throw new Error("The slip download failed. It's possible that the slip is not available because Punjab University hasn't uploaded it yet, you may not be registered, you may be dropped, or you may be alumni. Please check your registration status and try again later.");
+                throw new Error("Slip download failed. Please check your details and try again.");
             }
             return response.blob();
         })
         .then((blob) => {
             if (blob) {
-                // Create a link element
                 const link = document.createElement('a');
-                // Set the href attribute to the object URL of the blob
                 link.href = window.URL.createObjectURL(blob);
-                // Set the download attribute to specify the file name
-                link.download = `Slips ${rollNumber}.pdf`;
-                // Append the link to the document body
+                link.download = `Slips_${rollNumber}.pdf`;
                 document.body.appendChild(link);
-                // Trigger a click on the link to initiate the download
                 link.click();
-                // Remove the link from the document
                 document.body.removeChild(link);
-                // Release the object URL
                 window.URL.revokeObjectURL(link.href);
 
-                // Clear loading message
-                document.getElementById('resultMessage').innerHTML = '';
+                resultMessage.innerHTML = 'Download successful!';
                 clearForm();
             } else {
-                throw new Error("The slip download failed. It's possible that the slip is not available because Punjab University hasn't uploaded it yet, you may not be registered, you may be dropped, or you may be alumni. Please check your registration status and try again later.");
+                throw new Error("Slip download failed. Please try again later.");
             }
         })
         .catch((error) => {
             console.error('Error:', error.message);
-            // Display error message
-            document.getElementById('resultMessage').innerHTML = error.message;
+            alert(error.message); // Display error alert
+            resultMessage.innerHTML = '';
         });
 }
-
 
 function clearForm() {
     document.getElementById('rollNumber').value = '';
