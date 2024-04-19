185325d9c194ab6300ba21a919737ccb078c7391
Kanwar Adnan <107979019+KanwarAdnan@users.noreply.github.com>
2024-04-19 13:32:01 +0500

    fixed for first semester as well

--- script.js
+++ script.js
@@ -28,10 +28,15 @@ document.addEventListener('DOMContentLoaded', function () {
       }
    });
 });
-function validateRollNumber(rollNumber) {
-   const rollNumberPattern = /^[0-9]{5,6}$/;
-   return rollNumberPattern.test(rollNumber);
+
+function validateRollNumber(input) {
+   const rollNumberPattern = /^[0-9]{5,6}$/; // Matches 5 to 6 digits
+   const cnicPattern = /^\d{5}-\d{7}-\d$/; // Matches CNIC format XXXXX-XXXXXXX-X
+
+   // Check if input matches either roll number or CNIC pattern
+   return rollNumberPattern.test(input) || cnicPattern.test(input);
 }
+
 function downloadFile() {
    const rollNumberInput = document.getElementById('rollNumber');
    const rollNumber = rollNumberInput.value;
@@ -79,22 +84,51 @@ function downloadFile() {
 }
 
 
-function getApiUrl(slipType, rollNumber) {
+// function getApiUrl(slipType, rollNumber) {
+//    // Old API URL
+//    // const baseUrl = 'https://api_last-1-j0851899.deta.app/';
+
+//    const baseUrl = 'https://kanwaradnanpusms-vvicnw7txq-uc.a.run.app/';
+
+//    if (slipType === 'exam') {
+//       return `${baseUrl}download_slip`;
+//    } else if (slipType === 'adp') {
+//       return `${baseUrl}download_adp_slip`;
+//    } else if (slipType === 'prc') {
+//       return `${baseUrl}download_practical_slip`; // fixed
+//    }  else if (slipType === 'rt') {
+//       return `${baseUrl}download_result_transcript`; // fixed
+//    }
+
+// }
+
+function getApiUrl(slipType, identifier) {
    // Old API URL
    // const baseUrl = 'https://api_last-1-j0851899.deta.app/';
-
    const baseUrl = 'https://kanwaradnanpusms-vvicnw7txq-uc.a.run.app/';
+   const rollNumberRegex = /^\d+$/; // Regex to match digits only (for roll number)
+   const cnicRegex = /^\d{5}-\d{7}-\d$/; // Regex to match CNIC format (XXXXX-XXXXXXX-X)
+
+   // Determine if the provided identifier is a roll number or CNIC
+   const isRollNumber = rollNumberRegex.test(identifier);
+   const isCNIC = cnicRegex.test(identifier);
 
    if (slipType === 'exam') {
-      return `${baseUrl}download_slip`;
+      if (isRollNumber) {
+         return `${baseUrl}download_slip`;
+      } else if (isCNIC) {
+         return `${baseUrl}download_slip_using_cnic`;
+      }
    } else if (slipType === 'adp') {
       return `${baseUrl}download_adp_slip`;
    } else if (slipType === 'prc') {
-      return `${baseUrl}download_practical_slip`; // fixed
-   }  else if (slipType === 'rt') {
-      return `${baseUrl}download_result_transcript`; // fixed
+      return `${baseUrl}download_practical_slip`;
+   } else if (slipType === 'rt') {
+      return `${baseUrl}download_result_transcript`;
    }
 
+   // Return null if slipType is not recognized or identifier format is invalid
+   return null;
 }
 
 function clearForm() {
